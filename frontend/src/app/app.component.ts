9e583e9d4211cd10cf2cd8bc2aab6b15f0b7480e
Chingiz Egamberdiev <egamberdiev.chingiz@mail.ru>
2020-06-21 18:17:32 +0300

    uneccessary frontend commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'chingiz';
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  public ngOnInit(): void {
+  }
 }
