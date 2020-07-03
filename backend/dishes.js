cba06389ff176755c73618ffd8d8dbaca4f8211d
Chingiz Egamberdiev <egamberdiev.chingiz@mail.ru>
2020-07-03 10:02:05 +0300

    Add cart

--- backend/dishes.js
+++ backend/dishes.js
@@ -3,62 +3,152 @@ const dishes = [
         name: 'henkali',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'plov',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'hachapooree',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'caeser',
         quantity: 1,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'henkali',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'plov',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'hachapooree',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
 
     },
     {
         name: 'caeser',
         quantity: 1,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'henkali',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'plov',
         quantity: 3,
         price: 499,
-        weight: 300
+        weight: 300,
+        ingredients: [
+            'tomato',
+            'potato',
+            'cucamber',
+            'carrot',
+            'shinat',
+            'fish',
+            'chicken'
+        ]
     },
     {
         name: 'hachapooree',
