74c0db86a966db45ff98edf49e569e08d749bb30
Chingiz Egamberdiev <egamberdiev.chingiz@mail.ru>
2020-08-29 16:41:34 +0300

    admin section

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {objectNotEmpty} from '../shared/functions/general-use-functions';
 
 @Component({
   selector: 'app-admin',
@@ -8,14 +9,21 @@ import {HttpClient} from '@angular/common/http';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminComponent implements OnInit {
-  tableOrders: object;
+  tableOrders;
 
   constructor(private readonly http: HttpClient,
-              private readonly cd: ChangeDetectorRef) { }
+              private readonly cd: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.http.get('/api/database/getAll').subscribe(allOrders => {
-      this.tableOrders = allOrders;
+      this.tableOrders = objectNotEmpty(allOrders) ? allOrders : null;
+      this.cd.markForCheck();
+    });
+  }
+
+  backup(): void {
+    this.http.get('/api/cart/getBackup').subscribe(lastBackupData => {
+      this.tableOrders = lastBackupData;
       this.cd.markForCheck();
     });
   }
